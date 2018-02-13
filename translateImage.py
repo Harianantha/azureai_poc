12a547a207d38bdb866ee3744ea62c98ffb9731c
Hariharan Anantharaman <hariharan.anantharaman@techolution.com>
2018-02-13 20:46:54 +0530

    CHanges to handle pdf

--- translateImage.py
+++ translateImage.py
@@ -5,9 +5,10 @@ def main(argv):
     #translator = MultiFileTranslator()
     fileName = argv[1]
 
-    if(fileName. endswith ('.tiff') or fileName. endswith ('.tif' ) ):
+    if(fileName. endswith ('.tiff') or fileName. endswith ('.tif' ) or fileName.endswith ('.pdf' ) or fileName.endswith ('.PDF' ) ):
         translator = MultiFileTranslator()
         translator.translateFile(fileName)
+    #PDF, DOCX,JPEG/PNG
     else:
         translator = SingleFileTranslator()
         translator.translateFile(fileName)

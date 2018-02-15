6659304cb283501c87e75d1f3afb5aec82f5ddf3
Hariharan Anantharaman <hariharan.anantharaman@techolution.com>
2018-02-15 21:20:52 +0530

    Made translate image as class

--- translateImage.py
+++ translateImage.py
@@ -2,28 +2,29 @@ from  MultiFileTranslator import MultiFileTranslator
 from SingleFileTranslator import SingleFileTranslator
 import os
 
-def main(argv):
-    #translator = MultiFileTranslator()
-    fileName = argv[1]
+class translateImage:
+    def main(argv):
+        #translator = MultiFileTranslator()
+        fileName = argv[1]
 
-    if(fileName. endswith ('.tiff') or fileName. endswith ('.tif' ) or fileName.endswith ('.pdf' ) or fileName.endswith ('.PDF' ) ):
-        translator = MultiFileTranslator()
-        outputfile=translator.translateFile(fileName)
-        print('Access translated file at %s'%outputfile)
-    #PDF, DOCX,JPEG/PNG
-    elif (fileName. endswith ('.docx')):
-        command ='C:\Techolution\IOTPractise\CustomerProjects\Axa\DocumentTranslator\executeDocumentTranslator.bat '+fileName
-        output = os.system(command)
-        if (output == 0):
-            outputfilename=fileName.replace(".docx",".en.docx")
-            print ('output file of document translator %s'%outputfilename)
-    else:
-        translator = SingleFileTranslator()
-        outputfile=translator.translateFile(fileName)
-        print('Access translated file at %s'%outputfile)
+        if(fileName. endswith ('.tiff') or fileName. endswith ('.tif' ) or fileName.endswith ('.pdf' ) or fileName.endswith ('.PDF' ) ):
+            translator = MultiFileTranslator()
+            outputfile=translator.translateFile(fileName)
+            print('Access translated file at %s'%outputfile)
+        #PDF, DOCX,JPEG/PNG
+        elif (fileName. endswith ('.docx')):
+            command ='C:\Techolution\IOTPractise\CustomerProjects\Axa\DocumentTranslator\executeDocumentTranslator.bat '+fileName
+            output = os.system(command)
+            if (output == 0):
+                outputfilename=fileName.replace(".docx",".en.docx")
+                print ('output file of document translator %s'%outputfilename)
+        else:
+            translator = SingleFileTranslator()
+            outputfile=translator.translateFile(fileName)
+            print('Access translated file at %s'%outputfile)
 
 
 
-if __name__ == "__main__":
-    from sys import argv
-    main(argv)
+    if __name__ == "__main__":
+        from sys import argv
+        main(argv)
